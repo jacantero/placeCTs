8a0855581466e736526fa0dea9ed07e7f1309ed5
unknown <jesus_cantero95@hotmail.com>
2026-09-17 08:14:04 +0200

    Backup a main

--- main.py
+++ main.py
@@ -11,9 +11,5 @@ selected_rows = df[df['Capa'].isin(capas_validas)]
 
 potencias, positions = get_power(selected_rows)
 parcelas = get_parcelas(selected_rows)
-poligonos_cerrados = dict_parcelas(selected_rows, positions)
-parcelas_estructuradas = asignar_potencia_a_parcelas_cosidas(poligonos_cerrados, positions, potencias)
-
-plot_graph_cosido(parcelas_estructuradas, potencias, poligonos_cerrados)
-
 centros_cts, labels = place_CTs(potencias, positions)
+plot_graph(potencias, positions, parcelas, centros_cts, labels)
